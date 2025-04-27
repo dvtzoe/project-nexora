19b74c42965c49014d5b1bc6967f9c598d87dfaf
dvtzoe <ohmtzoe@gmail.com>
2025-04-27 14:41:22 +0700

    add sidebar auth nav

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -10,7 +10,7 @@ const navigations = [
 
 export default function Sidebar() {
   return (
-    <div className="bg-bg-secondary text-fg-primary border-neutral flex h-screen w-64 flex-col border-r">
+    <div className="bg-bg-secondary text-fg-primary border-neutral flex h-screen w-64 flex-col flex-nowrap overflow-auto border-r">
       {/* Logo */}
       <div className="border-neutral m-4 flex h-16 items-center justify-center border-b-2">
         <a className="text-fg-primary text-2xl font-bold italic" href="/">
@@ -33,6 +33,23 @@ export default function Sidebar() {
           </a>
         ))}
       </nav>
+      <div>
+        {/* Auth */}
+        <div className="m-4 flex flex-grow flex-col gap-4">
+          <a
+            href="/signin"
+            className="border-neutral hover:border-fg-secondary hover:text-fg-tertiary rounded-lg border-2 p-1 text-center font-medium transition-colors duration-300 ease-out"
+          >
+            Sign in
+          </a>
+          <a
+            href="/signup"
+            className="bg-button hover:bg-button-hover hover:text-fg-tertiary rounded-lg p-1 text-center font-medium transition-colors duration-300 ease-out"
+          >
+            Sign up
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
